ada1ce31f61e2c1333f3e58178683312e360b9b7
Stephen <xxw11165@gmail.com>
2021-01-01 00:25:29 -0800

    app init

---
+++ mini-amazon-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hello! Let's build the mini-amazon-app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ mini-amazon-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTEtiLn9zxXkpWnXU68HldKdFGZmjXEdE",
+  authDomain: "mini-6b1c3.firebaseapp.com",
+  projectId: "mini-6b1c3",
+  storageBucket: "mini-6b1c3.appspot.com",
+  messagingSenderId: "292396757914",
+  appId: "1:292396757914:web:6115e717330e1c2eed0023",
+  measurementId: "G-JQ4KZ90SN7",
+};
