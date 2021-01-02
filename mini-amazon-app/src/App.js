7ed9a432631ffccc91e045c1c55412ef6af38ebf
Stephen <xxw11165@gmail.com>
2021-01-01 17:48:45 -0800

    shopping cart checkout page

--- mini-amazon-app/src/App.js
+++ mini-amazon-app/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- mini-amazon-app/src/Product.js
+++ mini-amazon-app/src/Product.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  // console.log("This is the basket", basket);
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +36,7 @@ function Product({ id, title, image, price, rating }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
